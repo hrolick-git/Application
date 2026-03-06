94ca9d42611124a735c44cc8c0f2f5f0e43d93a7
Oleksandra Hladkova <alessandra.arisu@gmail.com>
2026-03-06 14:30:53 +0200

    Enable cors for railway

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({
-    origin: true,
+    origin: 'https://frontend-production-0751.up.railway.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
