43ae3cd7ccdc76e1842255d398a2278261f25299
Oleksandra Hladkova <alessandra.arisu@gmail.com>
2026-03-06 13:02:22 +0200

    Change vite config fot railway

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  allowedHosts: [
-    'frontend-production-0751.up.railway.app'
-  ],
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    // ПЕРЕНЕСИ СЮДИ:
+    allowedHosts: [
+      'frontend-production-0751.up.railway.app',
+      '.up.railway.app' // Додала про всяк випадок, щоб працювало на будь-якому піддомені Railway
+    ],
   },
   build: {
     rollupOptions: {
