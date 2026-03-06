f4ff7526eef1a4ac739efd87456a4b5aa07389c3
Oleksandra Hladkova <alessandra.arisu@gmail.com>
2026-03-06 12:48:40 +0200

    Allowed host on Railway at vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  allowedHosts: [
+    'frontend-production-0751.up.railway.app'
+  ],
   plugins: [react()],
   server: {
     port: 5173,
